f15272813fbe28bb1bbc044424f1f589d7ed5931
danica <liwandan@gmail.com>
2020-06-15 15:17:04 +1200

    splash activity

---
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.mobileassignment3.parcel_tracking_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+
+        setContentView(R.layout.activity_splash);
+
+        // add a timer that will trigger next activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
