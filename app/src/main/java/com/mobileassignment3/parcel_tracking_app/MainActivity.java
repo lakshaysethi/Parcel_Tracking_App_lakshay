6fe66fbb18758fe43f7860cc843bbaf0a9d08065
danica <liwandan@gmail.com>
2020-06-18 23:40:48 +1200

    notification ui

--- app/src/main/java/com/mobileassignment3/parcel_tracking_app/MainActivity.java
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/MainActivity.java
@@ -39,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         rvAssignOrder = findViewById(R.id.rvAssignOrder);
-
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         rvAssignOrder.setHasFixedSize(true);
--- app/src/main/java/com/mobileassignment3/parcel_tracking_app/NotificationActivity.java
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/NotificationActivity.java
@@ -1,14 +1,21 @@
 package com.mobileassignment3.parcel_tracking_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NotificationActivity extends AppCompatActivity {
-
+    private RecyclerView rvNotification;
+    private RecyclerView.Adapter adapterNotification;
+    private RecyclerView.LayoutManager layoutManagerNotification;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,30 @@ public class NotificationActivity extends AppCompatActivity {
         getSupportActionBar().setLogo(R.drawable.ic_person_pin_black_24dp);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setTitle("Username");
+
+        rvNotification = findViewById(R.id.rvNotification);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        rvNotification.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManagerNotification = new LinearLayoutManager(this);
+        rvNotification.setLayoutManager(layoutManagerNotification);
+
+        // specify an adapter
+        Notification[] myDataset = new Notification[] {
+                new Notification("Notification 1", "Beef inside"),
+                new Notification("Notification 2", "Countdown delivery"),
+                new Notification("Notification 3", "Dahua supermarket tuan gou"),
+                new Notification("Notification 4", "Beef inside"),
+                new Notification("Notification 5", "Countdown delivery"),
+                new Notification("Notification 6", "Dahua supermarket tuan gou"),
+                new Notification("Notification 7", "Beef inside"),
+                new Notification("Notification 8", "Countdown delivery"),
+                new Notification("Notification 9", "Dahua supermarket tuan gou"),
+        };
+        adapterNotification = new NotificationAdapter(myDataset);
+        rvNotification.setAdapter(adapterNotification);
     }
 
     // implemented the menu item
@@ -39,4 +70,64 @@ public class NotificationActivity extends AppCompatActivity {
         return(super.onOptionsItemSelected(item));
     }
 
+}
+
+class Notification {
+    public final String title;
+    public final String detail;
+
+    Notification(String title, String detail) {
+        this.title = title;
+        this.detail = detail;
+    }
+}
+
+class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.MyViewHolder> {
+    private Notification[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public CardView cardView;
+        public TextView textViewTitle;
+        public TextView textViewDetail;
+
+        public MyViewHolder(CardView v, TextView tv1, TextView tv2) {
+            super(v);
+            cardView = v;
+            textViewTitle = tv1;
+            textViewDetail = tv2;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public NotificationAdapter(Notification[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public NotificationAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        CardView v = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cards_notification, parent, false);
+        TextView title = (TextView) v.findViewById(R.id.cardNotificationTitle);
+        TextView detail = (TextView) v.findViewById(R.id.cardNotificationDetail);
+
+        MyViewHolder vh = new MyViewHolder(v, title, detail);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.textViewTitle.setText(mDataset[position].title);
+        holder.textViewDetail.setText(mDataset[position].detail);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
 }
