f43480895f67b452f93b2843485317e7e5a3cc79
danica <liwandan@gmail.com>
2020-06-18 14:44:49 +1200

    notification back to last page

--- app/src/main/java/com/mobileassignment3/parcel_tracking_app/NotificationActivity.java
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/NotificationActivity.java
@@ -2,7 +2,10 @@ package com.mobileassignment3.parcel_tracking_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -18,5 +21,22 @@ public class NotificationActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Username");
     }
 
+    // implemented the menu item
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notification, menu);
+        return true;
+    }
+    // implemented the menu item
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
+        case R.id.backArrow:
+            finish();
+            return(true);
+
+    }
+        return(super.onOptionsItemSelected(item));
+    }
 
 }
