f0b06eadb11344fb7f1bb8d1c33d60d6677c490b
Midlanr <42661870+Midlanr@users.noreply.github.com>
2020-06-23 22:44:14 +1200

    Updated admin and customer class

--- app/src/main/java/com/mobileassignment3/parcel_tracking_app/classes/user/Admin.java
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/classes/user/Admin.java
@@ -1,4 +1,12 @@
 package com.mobileassignment3.parcel_tracking_app.classes.user;
 
-public class Admin {
+import android.provider.ContactsContract;
+
+public class Admin extends User{
+
+    public Admin(ContactsContract.CommonDataKinds.Email email, String username, String password) {
+        super(email, username, password);
+    }
+
+    
 }
--- app/src/main/java/com/mobileassignment3/parcel_tracking_app/classes/user/Customer.java
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/classes/user/Customer.java
@@ -1,7 +1,15 @@
 package com.mobileassignment3.parcel_tracking_app.classes.user;
 
 import android.location.Address;
+import android.provider.ContactsContract;
 
-public class Customer {
+public class Customer extends  User{
     Address address;
+
+    public Customer(ContactsContract.CommonDataKinds.Email email, String username, String password, Address address) {
+        super(email, username, password);
+        this.address = address;
+    }
+
+
 }
