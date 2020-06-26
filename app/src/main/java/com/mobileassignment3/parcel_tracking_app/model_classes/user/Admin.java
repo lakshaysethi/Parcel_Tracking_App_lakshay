ec7ac25c6e4a7b4e93f056a33a3fcb80cf539b09
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-06-26 19:56:33 +1200

    Update Admin.java

--- app/src/main/java/com/mobileassignment3/parcel_tracking_app/model_classes/user/Admin.java
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/model_classes/user/Admin.java
@@ -2,9 +2,28 @@ package com.mobileassignment3.parcel_tracking_app.model_classes.user;
 
 import android.provider.ContactsContract;
 
+import com.mobileassignment3.parcel_tracking_app.model_classes.DeliveryJob;
+
+import java.util.List;
+
 public class Admin extends User{
 
-    
 
-    
+    List<DeliveryJob> deliveryJobList;
+
+    public Admin() {
+    }
+
+    public Admin(String email, String username) {
+        super(email, username);
+    }
+
+    public List<DeliveryJob> getDeliveryJobList() {
+        return deliveryJobList;
+    }
+
+    public void setDeliveryJobList(List<DeliveryJob> deliveryJobList) {
+        this.deliveryJobList = deliveryJobList;
+    }
+
 }
